c4a024a83e60c797ad87d8593ce9bddf32720b42
JSongProvision <childrenofthefenceunite@gmail.com>
2025-05-29 22:56:07 -0400

    Add buttons and start of event delegation
    Started the UI journey for the game.

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const choices = document.querySelector("#choices");
+
+choices.addEventListener('click', (event) => {
+    const target = event.target;
+
+    switch (target.id) {
+        case 'rock-btn':
+        
+            break;
+        case 'paper-btn':
+
+            break;
+        case 'scissors-btn':
+
+            break;
+    }
+})
+
 // global variables
 let computerScore = 0;
 let playerScore = 0;
@@ -60,12 +78,12 @@ function playRound () {
 
 function playGame () {
     
-    for (i=1; i <= 5; i++ ) {
+    // for (i=1; i <= 5; i++ ) {
         alert(`ROUND ${round}: FIGHT!`);
         console.log(`==ROUND ${round} RESULTS==`);
         playRound();
         round++;
-    }
+    // }
 
 }
 
@@ -87,38 +105,3 @@ if (computerScore > playerScore) {
     alert("This game is busted all to hell. Apparently you didn't lose, win, or tie. You exist in some sort of quantum lock like schrodingers cat. In really I created a really intersting bug! ");
     console.log("This game is busted all to hell. Apparently you didn't lose, win, or tie. You exist in some sort of quantum lock like schrodingers cat. In really I created a really intersting bug! ");
 }
-
-
-
-// Step 4: Declare the players score variables
-// Your game will keep track of the players score. You will write variables to keep track of the players score.
-
-// Create two new variables named humanScore and computerScore in the global scope.
-// Initialize those variables with the value of 0.
-// Step 5: Write the logic to play a single round
-// Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-
-// Create a new function named playRound.
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-// Increment the humanScore or computerScore variable based on the round winner.
-// Example code:
-
-// function playRound(humanChoice, computerChoice) {
-//   // your code here!
-// }
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humanSelection, computerSelection);
-// Step 6: Write the logic to play the entire game
-// Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
-
-// Create a new function named playGame.
-// Move your playRound function and score variables so that they’re declared inside of the new playGame function
-// Play 5 rounds by calling playRound 5 times.
-// Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
-// Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
-// If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
