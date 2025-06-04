50f79f8d0ce2a004fe5f1cb8fff1226616ae2606
JSongProvision <childrenofthefenceunite@gmail.com>
2025-06-03 23:12:22 -0400

    Clean up code
    Removed commented out code and alert, fixed spacing

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let computerScore = 0;
 let playerScore = 0;
 let round = 1;
 
+// listen for user click on buttons to set user choice and play the game
 choices.addEventListener('click', (event) => {
     const target = event.target;
     let playerChoice = 'Never Set';
@@ -39,7 +40,6 @@ choices.addEventListener('click', (event) => {
     }        
 });
 
-
 //generate random computer choice
 function getComputerChoice() {
     random = Math.floor(Math.random() * 3);
@@ -58,49 +58,25 @@ function getComputerChoice() {
     }          
 }
 
-// pass getComputerChoice and getHumanChoice as parameters to determine a winner in a given round
+//determine a winner in a given round
 function determineWinner (computerPick,playerPick) {
     
     if (computerPick === playerPick) {
-            // alert( `Tie! You both picked ${computerPick}`);
             return `Tie! You both picked ${computerPick}`;
     }   else if (computerPick === "Rock" && playerPick === "Scissors" || computerPick === "Paper" && playerPick === "Rock" || computerPick === "Scissors" && playerPick == "Paper") {
             computerScore++;
-            // alert( `You are a giant loser! ${computerPick} beats ${playerPick}.`);
             return `You are a giant loser! ${computerPick} beats ${playerPick}.`;
     }   else if (playerPick === "Rock" && computerPick === "Scissors" || playerPick === "Paper" && computerPick === "Rock" || playerPick === "Scissors" && computerPick == "Paper") {
             playerScore++;
-            // alert( `Congrats, you won! ${playerPick} beats ${computerPick}. I bet your dad is proud of you ... wherever he is... `);
             return `Congrats, you won! ${playerPick} beats ${computerPick}. I bet your dad is proud of you ... wherever he is... `;
     }   else {
-            // alert("Something has gone horribly wrong and the game is rigged!");
             return "Something has gone horribly wrong and the game is rigged!";
     }
 }
 
-// start a round of the game invoking determine winner function and logging results of the round and the current score standings to user
+// start a round of the game 
 function playRound (playerChoice) {
 
     resultsDisplay.textContent = determineWinner( getComputerChoice() , playerChoice );
     scoreDisplay.textContent = `The computer's score is : ${computerScore} and your score is: ${playerScore}`;
 }
-
-// function playGame () {}
-
-// playGame();
-
-// console.log(`==[GAME OVER]==
-//         FINAL SCORE - Computer: ${computerScore} Player: ${playerScore}`);
-// if (computerScore > playerScore) {
-//     alert("The computer beat you! You are the suck!");
-//     console.log("The computer beat you! You are the suck!");
-// } else if (playerScore > computerScore) {
-//     alert("YOU WIN THE GAME! I mean, you beat a computer in a game of chance, is that really a victory?");
-//     console.log("YOU WIN THE GAME! I mean, you beat a computer in a game of chance, is that really a victory?");
-// } else if (playerScore === computerScore) {
-//     alert("Its a straight DRAW! I guess you both suck.");
-//     console.log("Its a straight DRAW! I guess you both suck.");
-// } else {
-//     alert("This game is busted all to hell. Apparently you didn't lose, win, or tie. You exist in some sort of quantum lock like schrodingers cat. In really I created a really intersting bug! ");
-//     console.log("This game is busted all to hell. Apparently you didn't lose, win, or tie. You exist in some sort of quantum lock like schrodingers cat. In really I created a really intersting bug! ");
-// }
